dee0abc357d4bd263a428e873ec92347bff281c4
Tommy Alpert <tommy0356@gmail.com>
2021-05-17 12:18:39 -0400

    Search functionality for artist and songs

--- App.js
+++ App.js
@@ -3,26 +3,10 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CreateSong from './src/components/CreateSong';
-// import SearchSong from './src/components/SearchSong';
-// import SongList from './src/components/SongList';
+import SearchSong from './src/components/SearchSong';
+import SongList from './src/components/SongList';
 
 const Tab = createBottomTabNavigator();
-
-function SongList() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Song List Goes Here</Text>
-      </View>
-    );
-  }
-
-function SearchSong() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Search Songs Goes Here</Text>
-      </View>
-    );
-  }
 
 export default function App() {
   return (
--- src/components/SearchSong.js
+++ src/components/SearchSong.js
@@ -1,12 +1,138 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, StyleSheet, View, FlatList, Button } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 function SearchSong() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Search Songs Goes Here</Text>
-      </View>
-    );
-  }
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+  const [searchBySongs, setSearchBySongs] = useState(true)
 
-  export default SearchSong;
+  useEffect(() => {
+    fetch('http://localhost:8000/api/songs/')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson);
+        setMasterDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource
+      // Update FilteredDataSource
+      var newData;
+      if (searchBySongs) {// Search by song
+         newData = masterDataSource.filter(function (item) {
+            const itemData = item.song
+            ? item.song.toUpperCase()
+            : ''.toUpperCase();
+            const textData = text.toUpperCase();
+            return itemData.indexOf(textData) > -1;
+        });
+      } else {
+         newData = masterDataSource.filter(function (item) {
+            const itemData = item.artist
+            ? item.artist.toUpperCase()
+            : ''.toUpperCase();
+            const textData = text.toUpperCase();
+            return itemData.indexOf(textData) > -1;
+        });
+      }
+      setFilteredDataSource(newData);
+      setSearch(text)
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text)
+    }
+  };
+
+  const handleSearchChange = (selectSong) => {
+    setSearchBySongs(selectSong)
+    searchFilterFunction('')
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {"Song: "}
+        {item.song}
+        {'    Artist: '}
+        {item.artist}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+  const getItem = (item) => {
+    // Function for click on an item
+    alert('Song: ' + item.song + ' Artist: ' + item.artist);
+  };
+
+  // TODO: show button is selected, spacing at the top for Android, correct spacing for search horizontal stack
+return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <SearchBar
+          round
+          searchIcon={{ size: 24 }}
+          onChangeText={(text) => searchFilterFunction(text)}
+          onClear={(text) => searchFilterFunction('')}
+          placeholder="Type Here..."
+          value={search}
+        />
+        <View style={styles.searchByButtons}>
+          <Button title="Search By Song"
+              onPress={() => handleSearchChange(true)}
+          />
+          <Button color='#ff0000' title="Search By Artist"
+              onPress={() => handleSearchChange(false)}
+          />
+        </View>
+        <FlatList
+          data={filteredDataSource}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+  itemStyle: {
+    padding: 10,
+  },
+  searchByButtons: {
+    backgroundColor: 'white',
+    flexDirection: "row",
+    padding: 10,
+    justifyContent: 'space-around'
+  }
+});
+
+export default SearchSong;
