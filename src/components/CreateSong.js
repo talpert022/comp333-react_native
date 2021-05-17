78b780fcbc56aa0eafeafea4fe7a7b9cab9174d4
matthewquerdasi <matthew.querdasi@gmail.com>
2021-05-17 13:13:56 -0400

    inital work on createSong

--- src/components/CreateSong.js
+++ src/components/CreateSong.js
@@ -1,10 +1,85 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, TextInput, StyleSheet, Button, Text, Alert, View } from 'react-native';
+import axios from "axios";
+
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+});
+
 
 export default function CreateSong() {
+    var [song, setSong] = React.useState({
+      song: ''
+    });
+
+    var [artist, setArtist] = React.useState({
+      artist: ''
+    });
+
+    var [username, setUsername] = React.useState({
+      username: ''
+    });
+
+    var [rating, setRating] = React.useState({
+      rating: 5
+    });
+
+    var state = {
+      song, 
+      artist,
+      username,
+      rating
+    }
+
+    const handleSubmit = event => {
+      var ratingData = JSON.stringify({username: state.username, song: state.song, rating: state.rating});
+      axios.post('http://localhost:8000/api/ratings/', ratingData)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+
+      var songData = JSON.stringify({song: state.song, artist: state.artist});
+      axios.post('http://localhost:8000/api/songs/', songData);
+      
+    }
+    
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>TEST TEST TEST </Text>
-      </View>
+      <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Rate a song!</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setSong}
+          placeholder="Song"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setArtist}
+          placeholder="Artist"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setUsername}
+          placeholder="Username"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setRating}
+          placeholder="Rating"
+          keyboardType="numeric"
+        />
+        <Button
+          title="Press me"
+          onPress={handleSubmit}
+        />
+      </SafeAreaView>
     );
   }
